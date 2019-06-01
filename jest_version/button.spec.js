453e081b25ef5dd44e5ae70479a6fa1f9b2f779c
Rudy <RodriguezLucha@gmail.com>
2019-06-01 06:03:11 -0700

    Add next failing test

--- jest_version/button.spec.js
+++ jest_version/button.spec.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import ReactTestUtils from 'react-dom/test-utils';
+
 import Button from './button';
 
 
@@ -14,4 +16,21 @@ test('renders with text', () => {
   const button = renderer.getRenderOutput();
   expect(button.type).toBe('button');
   expect(button.props.children).toBe(text);
+});
+
+test('fires the onClick callback', () => {
+  const onClick = jest.fn();
+
+  const tree = ReactTestUtils.renderIntoDocument(
+    <Button onClick={onClick} />
+  );
+
+  const button = ReactTestUtils.findRenderedDOMComponentWithTag(
+    tree,
+    'button'
+  );
+
+  ReactTestUtils.Simulate.click(button);
+
+  expect(onClick).toBeCalled();
 });
