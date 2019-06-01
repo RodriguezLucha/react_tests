a1168da51ad2b19e80727f4221c938f2724b443b
Rudy <RodriguezLucha@gmail.com>
2019-06-01 05:15:26 -0700

    Pass first test

--- jest_version/button.jsx
+++ jest_version/button.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class Button extends React.Component {
   render() {
     return (
-      <div />
+      <button>
+        {this.props.text}
+      </button>
     );
   }
 }
